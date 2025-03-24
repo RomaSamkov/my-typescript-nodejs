e115a4d94135654f5dd16c34677759c4caa1d56e
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-24 20:39:55 +0200

    Create users.ts

---
+++ src/controllers/users.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { User } from "../types/response";
+
+export const getUsers = async (req: Request, res: Response) => {
+  res.send([]);
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  res.send({});
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  res.send({
+    id: 1,
+    name: "Kevin",
+    email: "kevin@mail.com",
+  });
+};
