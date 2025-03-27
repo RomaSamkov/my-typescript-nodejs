cc0be86afb06728cbb5fd379bf8286ef30cd0208
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-27 16:36:51 +0200

    add process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import usersRouter from "./routes/users";
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Welcome to Express TypeScript");
+  res.send("Welcome to Express TypeScript.");
 });
 
 app.use("/api/users", usersRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
